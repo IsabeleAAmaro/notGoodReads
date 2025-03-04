fe94f9449ad1f4079bd4e75739913d3960b5a4c3
isabele <isabeleamaro@edu.unirio.br>
2025-03-03 23:22:53 -0300

    Adicionados novos arquivos necessarios pra login com autenticaçao JWT

---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,27 @@
+// frontend/src/context/AuthContext.jsx
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const checkAuth = async () => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      setUser({ username: 'Usuário' });
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
